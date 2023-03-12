27985ddf2724c0cbed3723379a07d5f95a490054
Angelo Pirola <angelo9871@gmail.com>
2023-03-12 15:24:19 +0100

    Aggiunto Authentication Provider

--- src/GestioneSagre.App/GlobalUsings.cs
+++ src/GestioneSagre.App/GlobalUsings.cs
@@ -1,3 +1,7 @@
-﻿global using GestioneSagre.App;
+﻿global using System.IdentityModel.Tokens.Jwt;
+global using System.Security.Claims;
+global using Blazored.LocalStorage;
+global using GestioneSagre.App.Services;
+global using Microsoft.AspNetCore.Components.Authorization;
 global using Microsoft.AspNetCore.Components.Web;
 global using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
--- src/GestioneSagre.App/Program.cs
+++ src/GestioneSagre.App/Program.cs
@@ -1,7 +1,23 @@
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
+namespace GestioneSagre.App;
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+public class Program
+{
+    public static async Task Main(string[] args)
+    {
+        //Riferimento: https://github.com/AngeloDotNet/WebApi.IdentityJWT
 
-await builder.Build().RunAsync();
+        var builder = WebAssemblyHostBuilder.CreateDefault(args);
+        builder.RootComponents.Add<App>("#app");
+        builder.RootComponents.Add<HeadOutlet>("head::after");
+
+        builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+        builder.Services.AddBlazoredLocalStorage();
+        builder.Services.AddAuthorizationCore();
+
+        builder.Services.AddScoped<AppAuthenticationStateProvider>();
+        builder.Services.AddScoped<AuthenticationStateProvider>(provider => provider.GetRequiredService<AppAuthenticationStateProvider>());
+
+        await builder.Build().RunAsync();
+    }
+}
